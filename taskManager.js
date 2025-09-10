abd93ff5ffaad7215802990e083d8a12bc90bfc4
MarcusW <mrvwarnert@gmail.com>
2025-09-09 21:34:17 -0500

    Yup

---
+++ taskManager.js
@@ -0,0 +1,10 @@
+function addTask (tasks, task) {
+    tasks.push(task);
+    return tasks;
+}
+function listTasks(tasks) {
+    for (let arrayElement of tasks) {
+        console.log(arrayElement.task)
+    }
+}
+module.exports = {addTask : addTask , listTasks : listTasks };
